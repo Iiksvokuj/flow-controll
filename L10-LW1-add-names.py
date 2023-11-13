96df2cc9d6eac2ab03a5c7f86437ae587189a4b2
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-14 00:17:23 +0200

    names list sptlitted in rows

--- L10-LW1-add-names.py
+++ L10-LW1-add-names.py
@@ -2,12 +2,31 @@
 
 
 people = ""
-
+count = 0
 while True:
-    name = input("enter someone's name >> ")  #str <"John"> <"Mary">
+    count += 1
+    name = input(str(count) + ") enter someone's name >> ")  #str <"John"> <"Mary">
     if name == "":
         break
-    people += name + " "
+    people += name
+
+    if count % 3 == 0:
+        people += "\n"
+    else:
+        people += " "
 
 
+print()
 print(people)
+
+# "John Marry Peter\nJack Jane Victoria\nBob "
+
+
+# HW1:
+
+#
+#-----------------------------------------
+#|John         |Marry        |Peter      |   
+#|Tatyana      |Victoria     |Bob        |
+#|Jack         |Silvia       |           |
+#-----------------------------------------
