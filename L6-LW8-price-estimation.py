f83a1a1b277706d1aa0507f7dbec595b30e204ab
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-15 22:32:07 +0200

    5th step

--- L6-LW8-price-estimation.py
+++ L6-LW8-price-estimation.py
@@ -3,11 +3,11 @@ price = int(input('how much does it cost ? '))
 if price < 0:
     print("Price cannot be less then ZERO!!!")
 #    quit() # exit this script
+
+# x ------- 10 ----- 50 ------>
+elif price <= 10:
+    print("Cheap! :)")
+elif price <= 50:
+    print("Ok! :|")
 else:
-    # x ------- 10 ----- 50 ------>
-    if price <= 10:
-        print("Cheap! :)")
-    elif price <= 50:
-        print("Ok! :|")
-    else:
-        print("Expensive! :(")
+    print("Expensive! :(")
