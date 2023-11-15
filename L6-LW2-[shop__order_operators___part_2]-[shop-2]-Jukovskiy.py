f62888d667d04332eea8366408b359bd79ed2a8a
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-15 22:15:56 +0200

    some changes

--- L6-LW2-[shop__order_operators___part_2]-[shop-2]-Jukovskiy.py
+++ L6-LW2-[shop__order_operators___part_2]-[shop-2]-Jukovskiy.py
@@ -1,4 +1,21 @@
 
+# 1. free delivery? (yes/no - True/False)
+#     - client wants delivery ?
+#     - vip ?
+#     - cost >= 500.00 ?
+
+# delivery_free_limit = 500.00
+# delivery_cost       = 100.00
+# delivery_wanted_str = input('do you want delivery (yes/no) ? ')
+# client_is_vip       = False
+# order_cost          = 600.00
+
+#############
+
+# delivery_free_condition_wants = delivery_wanted_str == 'yes'
+# delivery_free_condition_vip   = client_is_vip
+# delivery_free_condition_cost  = order_cost >= delivery_free_limit
+
 # 2. order completion (order flow) ?  (yes/no - True/False)
 #     - select food
 #     - confirm order (online)
@@ -8,11 +25,18 @@
 #     - delivery (physical)
 #     - client satisfied
 
+## Programm Start
+
 select_food = False
 
 food_1_name = "Pizza"
 food_1_price = 75.00
-
+#####
+delivery_free_limit = 500.00
+delivery_cost       = 100.00
+client_is_vip       = False
+order_cost          = 600.00
+#####
 
 print ("do you want", food_1_name, "(yes/no)?")
 food_1_confirm = input()
@@ -31,3 +55,15 @@ if food_1_confirm == 'yes':
         pass
         # HW2:
         #     embed here delivery logic
+        delivery_wanted_str = input('do you want delivery (yes/no) ? ')
+        delivery_free_condition_wants = delivery_wanted_str == 'yes'
+        delivery_free_condition_vip   = client_is_vip
+        delivery_free_condition_cost  = food_1_cost >= delivery_free_limit
+        delivery_is_free    = delivery_free_condition_wants\
+                              and\
+                             (delivery_free_condition_vip or delivery_free_condition_cost)
+        delivery_is_free and print ("You've got free delivery.", delivery_is_free)
+        not delivery_is_free and print ("You've got to pay", delivery_cost, "for delivery.")
+
+
+
