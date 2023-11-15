70f9e65a6c16eb4e0bd1f8c690cdf4e40078a64e
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-15 22:14:28 +0200

    some chenges

--- L6-LW1-[shop__order_operators___part_1]-[shop]-Jukovskiy.py
+++ L6-LW1-[shop__order_operators___part_1]-[shop]-Jukovskiy.py
@@ -7,19 +7,21 @@
 #     - cost >= 500.00 ?
 
 delivery_free_limit = 500.00
-
+delivery_cost       = 100.00
 delivery_wanted_str = input('do you want delivery (yes/no) ? ')
 client_is_vip       = False
 order_cost          = 600.00
 
 ##############
+
 delivery_free_condition_wants = delivery_wanted_str == 'yes'
 delivery_free_condition_vip   = client_is_vip
 delivery_free_condition_cost  = order_cost >= delivery_free_limit
 
 delivery_is_free    = delivery_free_condition_wants and (delivery_free_condition_vip or delivery_free_condition_cost)
 #                                   False                           False                         True
-print ("You've got free delivery.", delivery_is_free)
+delivery_is_free and print ("You've got free delivery.", delivery_is_free)
+not delivery_is_free and print ("You've got to pay", delivery_cost, "for delivery.")
 
 # 2. order completion (order flow) ?  (yes/no - True/False)
 #     - select food
