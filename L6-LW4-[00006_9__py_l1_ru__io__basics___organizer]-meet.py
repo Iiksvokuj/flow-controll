c3a2cd2aa257ef40346034c2db5b7a5ebf057282
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-15 22:22:38 +0200

    start

---
+++ L6-LW4-[00006_9__py_l1_ru__io__basics___organizer]-meet.py
@@ -0,0 +1,18 @@
+# types
+# operators
+# i/o
+
+# DATA INPUT
+#  ┌---------┐ str
+#  v         ^
+e_what  = input("Event type: ") #e_what-event
+# 'Meet'
+e_where = input("Event location: ")
+# 'online'
+e_when  = input("Event tyme: ")
+# '20'
+
+# DATA OUTPUT
+print("\n\n\n--------------------------------")
+print("ATTANTION !!!")  # "ATTANTION !!! + \n"
+print("You have a", e_what, e_where, "at", e_when)
