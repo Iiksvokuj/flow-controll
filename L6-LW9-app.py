c05ece91692d891f7c7a5c3d789646b0be5b84dc
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-15 22:34:20 +0200

    homework

--- L6-LW9-app.py
+++ L6-LW9-app.py
@@ -1,13 +1,17 @@
 # Solving the situation when the client wants to pay for a service
-# - trying to pay by a cach
+# - trying to pay by a cash
 # - trying to pay by a credit card
 # - trying to pay by cryptocurrency
 
-service_price = 100
+# HW:
+#    - updae the flowchart
+#   *- maybe convert crypto to cash / card
 
-client_cash_amount = 50
-client_card_amount = 50
-client_card_crypto = 300
+service_price = 100  #EUR
+data_EUR_to_BTC = 0.0000288
+data_BTC_to_EUR = 34700
+
+client_cash_amount = int(input('Enter cash amount, EUR: '))
 
 
 if client_cash_amount >= service_price:
@@ -15,12 +19,17 @@ if client_cash_amount >= service_price:
 else:
 
     print('CASH payment failure!!!')
+    client_card_amount = int(input('Enter card amount, EUR: '))
     if client_card_amount >= service_price:
         print('CARD payment success!!!')
     else:
 
         print('CARD payment failure!!!')
-        if client_card_crypto >= service_price:
+        client_card_crypto = int(input('Enter crypto amount, BTC: '))
+        client_card_euro = client_card_crypto * data_BTC_to_EUR
+        if client_card_euro >= service_price:
+            print("Current rate is: 1 BTC = 34700 EUR")
+            print("In conversion to EUR you've entered the following amount: ", client_card_euro, "EUR")
             print('CRYPTO payment success!!!')
         else:
             print('CRYPTO payment failure!!!')
