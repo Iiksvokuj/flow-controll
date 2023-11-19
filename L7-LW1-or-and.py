813540cb91051377e831c166d3fe058b41b480ab
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-20 01:20:31 +0200

    L7-LW1 + HW1

---
+++ L7-LW1-or-and.py
@@ -0,0 +1,46 @@
+# or / and
+#
+# exp_1 ?       op_bin      exp_2 ?
+#    ^            ^            ^
+#    |            |            |
+# 1. eval left    |            |
+#                 |        2. eval right
+#                 |
+#              3. exec
+#              4. result
+
+
+# |   ALGO
+# |  1. eval left
+# |     1.b truthy (or), falthy (and) --------+
+# |  2. eval right                            |
+# |     2.b truthy (or), falthy (and) ----+   |
+# |  3. exec                              |   |
+# |  4. result                            |   |
+# v                                       v   v
+
+
+# x => [-5 .. -3] or [10 ... 20]
+
+# x = -4
+# r = x >= -5 and x <= -3 or x >= 10 and x <= 20
+#       |      |     |      |                   |
+#       v      |     |      +------- Skip ------+  
+#    1.True    |     v     
+#              |  2.True   
+#              v            
+#            3.True              
+#                       
+
+#################################################
+
+# HW1: try to explain how it works for:
+# x = -10
+# r = x >= -5 and x <= -3 or x >= 10 and x <= 20
+#       |            |     |    |           |
+#       v            |     |    |           |
+#    1.False         v     |    |           |
+#                 2.Skip   |    v           |
+#                          | 3.False        v
+#                          v             4.Skip
+#                       5.False
