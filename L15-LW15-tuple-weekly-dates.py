22e32386dd3126ae9575b3aa80af0cf7868f4273
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-20 20:47:55 +0200

    step 4: tuple VS list

--- L15-LW15-tuple-weekly-dates.py
+++ L15-LW15-tuple-weekly-dates.py
@@ -3,12 +3,22 @@
 #     type: list
 #       Mo
 #       v
-week = [20,21,22,23,24,25,26,]
+week = 20,21,22,23,24,25,26  #or week = (20,21,22,23,24,25,26) -> tuple
+week = tuple(week)
+
 print("type of variable type ", type(week))
 
 # modify
-week[6] = 31
+# week[6] = 31
 
 ###########################################
 for day in week:
     print (day)
+
+## tuple       <>      list 
+#    ^                   ^
+#  read-only          read/write/append/remove/sort/....
+#   - for in              - for in
+#   - in                  - in
+#   - [index]             - [index]      
+#     
