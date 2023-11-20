a4d9e67ecf2e9764ea31a731efe020fa80e8d16e
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-20 15:27:05 +0200

    1st step

---
+++ L7-LW2-salary.py
@@ -0,0 +1,11 @@
+# INPUT monthly salary LIMIT : 2000
+salary = 1000     #USD
+
+# x ----------------X--------------------> salary   
+# 0                2000                + infinity
+
+# ----> good
+if salary >= 2000:
+    print("good")
+else:
+    print("bad")
