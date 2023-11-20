373f8aa42ba282287d27ff0ffca46c3c51f8f5b0
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-20 15:40:03 +0200

    2nd step

--- L7-LW2-salary.py
+++ L7-LW2-salary.py
@@ -1,11 +1,18 @@
 # INPUT monthly salary LIMIT : 2000
 salary = 1000     #USD
 
-# x ----------------X--------------------> salary   
-# 0                2000                + infinity
+# 2000+         - good
+# 1000 .. 2000  - normal
+# 999-          - bad
+
+# x --------------x---------------X--------------------> salary   
+# 0      bad     1000    norm    2000     good       + infinity
 
 # ----> good
 if salary >= 2000:
     print("good")
 else:
-    print("bad")
+    if salary >= 1000:
+        print("normal")
+    else:
+        print("bad")
