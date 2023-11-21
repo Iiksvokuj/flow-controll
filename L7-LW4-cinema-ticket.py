7c27540476f850b54b4a303ddfbe32d3c22e5eb8
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-21 22:07:25 +0200

    step 2: addet string formatting:  f {  }

--- L7-LW4-cinema-ticket.py
+++ L7-LW4-cinema-ticket.py
@@ -5,20 +5,26 @@ from os import system
 
 # flow control: if / else
 
+# DATA
+m_title_1 = "Avatar 2"
+m_title_2 = "Terminator 9"
+m_title_3 = "Titanic Zombie"
+
+# HW1: insert other data as variable
 
 ### MOVIE BOARD
 system('cls')
 print(
-"""
-1. Avatar 2:
+f"""
+1. {m_title_1}:
    a. 18:00
    b. 20:00
 
-2. Terminator 9:
+2. {m_title_2}:
    a. 16:00
    b. 23:00
 
-3. Titanic Zombie:
+3. {m_title_3}:
    a. 18:00
 """    
 )
@@ -26,7 +32,7 @@ print(
 moovie_number = input("Choose a moovie: ")
 
 if moovie_number == '1':
-    print('You have chosen "Avatar 2"')
+    print(f'You have chosen "{m_title_1}"')
     time = input('Choose time: ')
     if time == 'a':
         print('time: 18:00, ticket cost 100')
@@ -34,7 +40,7 @@ if moovie_number == '1':
         print('time: 20:00, ticket cost 120')
 
 if moovie_number == '2':
-    print('You have chosen "Terminator 9"')
+    print(f'You have chosen "{m_title_2}"')
 
 if moovie_number == '3':
-    print('You have chosen "Titanic Zombie"')
+    print(f'You have chosen "{m_title_3}"')
