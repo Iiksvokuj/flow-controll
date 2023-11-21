1cb0acb34ffc85bfe9bc2c6dd7123851c56f47cb
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-21 15:23:22 +0200

    step 1

---
+++ L7-LW3-phone-call.py
@@ -0,0 +1,20 @@
+## can we make a call ?
+
+# factor 1
+CHARGE_LIMIT  = 2    # %
+charge        = 90   # %
+
+# factor 2
+BALANCE_LIMIT = -50 # $
+balance       = 0   # $
+
+if charge >= CHARGE_LIMIT:
+    print("we can make a call.")
+else:
+    print("please charge!")
+
+
+if balance >= BALANCE_LIMIT:
+    print("we can make a call.")
+else:
+    print("please pay!")
