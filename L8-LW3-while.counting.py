a95396b4518d1c8a24c57b546e8f1376c2d4f173
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-27 20:37:46 +0200

    [Video 3]

---
+++ L8-LW3-while.counting.py
@@ -0,0 +1,16 @@
+# count from 1 to 10 - > while
+
+# 1. start
+start = 1
+
+# 2. end
+end   = 10
+
+# 3. step + 1
+step  = 1
+
+# ###############
+n = start     # counter
+while n <= end:          # 11 times
+    print(n)
+    n += step            # 10 times
