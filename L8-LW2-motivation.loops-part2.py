fe06b075236f8ae86e9dfef009fd496f2bf8bc68
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-27 15:01:43 +0200

    endless cycle

--- L8-LW2-motivation.loops-part2.py
+++ L8-LW2-motivation.loops-part2.py
@@ -7,67 +7,14 @@ consumptionRate = 10  #%/m
 steps              = 0   # m
 step               = 1   # m
 
+# DRY - Don't Repeat Yourself
 # 2. ITERATIVE - SIMULATION
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
+while True:
+    # ######## STEP - BEGIN ########
+    steps          +=  step
+    batteryCharge  -=  consumptionRate
+    print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
+    if batteryCharge <= 10:
+        print("WARNING!!! Need to charge")
+    # ######## STEP - END   ########
 
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
-
-# ######## STEP - BEGIN ########
-steps          +=  step
-batteryCharge  -=  consumptionRate
-print("STEPS: ", steps, " CHARGE: ", batteryCharge, "%")
-if batteryCharge <= 10:
-    print("WARNING!!! Need to charge")
-# ######## STEP - END   ########
