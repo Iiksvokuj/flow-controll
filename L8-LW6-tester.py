694c0a8a378b9243abe95fda897456a2a196dd96
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-03 01:32:37 +0200

    added loop

--- L8-LW6-tester.py
+++ L8-LW6-tester.py
@@ -4,15 +4,23 @@
 # X---------v---...---v---...---v--->
 # 0         1         10       100
 
-####### ALGO ##########
-distance_km = float(input('Enter distance (km): '))
 
-print("The distance", distance_km,"km\nis:")
-if distance_km <= 1:
-    print(" - near")
-elif distance_km <= 10:
-    print(" - close")
-elif distance_km <= 100:
-    print(" - far")
-else:
-    print(" - very far")
+####### Testing case
+distance_km = -1
+while distance_km <= 101:
+
+    ####### ALGO ##########
+    # distance_km = float(input('Enter distance (km): '))
+    print("The distance", distance_km,"km\nis:")
+    if distance_km <= 1:
+        print(" - near")
+    elif distance_km <= 10:
+        print(" - close")
+    elif distance_km <= 100:
+        print(" - far")
+    else:
+        print(" - very far")
+    ####### ALGO ##########
+    
+    distance_km += 1
+####### Testing case
