062067458b890308e2cd16e56424a8c1278de215
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-04 23:58:58 +0200

    step 1: str formatting

---
+++ L8-LW7-pocket-calcultor.py
@@ -0,0 +1,15 @@
+# interactive calculator
+
+# CLI - textual terminal
+
+# input values
+# *operations: +, -, /, *
+# show result
+# * interactive menu
+
+# + print formatting
+
+print("",1000)
+print(" ",100)
+print("  ",10)
+print("   ",1)
