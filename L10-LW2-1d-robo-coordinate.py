e9dc94f9eb30c81711c5117dedc675d99685fb98
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-13 22:52:43 +0200

    1

---
+++ L10-LW2-1d-robo-coordinate.py
@@ -0,0 +1,15 @@
+# horisontal path / length
+# x1 --------------R-----------------------> x2
+# 0                                         n
+# roboX - robot coordinate
+
+length = 20
+roboX = 5
+
+x = 1
+print("\n")
+while x <= length:
+    print("-", end = "")    #\n
+    x += 1
+
+print("\n")
