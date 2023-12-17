e8ee978d9483932b31028f90e8b0abb781b7ddc2
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-17 17:50:41 +0200

    1

---
+++ L10-LW2-HW1_2-1d-robo-coordinate.py
@@ -0,0 +1,24 @@
+# horisontal path / length
+# x1 --------------R-----------------------> x2
+# 0                                         n
+# roboX - robot coordinate
+
+# HW1: input lenght/roboX
+#      check limits
+
+length = int(input("Enter the lenght of traectory [1 - 20]: ")) #20
+roboX = int(input("Enter robot coordinates [1 - 20]: "))        # 5
+if 1 <= (roboX and length) <= 20 :
+    x = 1
+    print("\n")
+    while x <= length:
+        # HW2: optimize from here
+        if x == roboX:
+            print("R", end = "")    #\n
+        else:
+            print("-", end = "")    #\n
+        # to here
+        x += 1
+    print("\n")
+else:
+    print("\nIncorrect Coordinates!\n")
