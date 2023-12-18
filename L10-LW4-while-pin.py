d602e32daa736c1a7c6eabf5f548f2dc4d3d20aa
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-19 00:04:32 +0200

    2: while cycle

--- L10-LW4-while-pin.py
+++ L10-LW4-while-pin.py
@@ -1,11 +1,14 @@
 # allow user > enter PIN > true
 
 correct_PIN = 1234
+wrong       = True
 
+# HW1: stop loop after 3 WRONG attempts
 
-while True:
+while wrong:
     user_PIN = int(input("Enter PIN: "))
     if user_PIN == correct_PIN:
         print("Yes!!!")
+        wrong = False
     else:
         print("Wrong!!!")
