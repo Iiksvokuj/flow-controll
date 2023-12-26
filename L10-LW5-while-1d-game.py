0feb8dbb256f2a6127bc5d315fbd77267508f612
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-26 22:10:45 +0200

    3: die Mine wird hinzugefügt

--- L10-LW5-while-1d-game.py
+++ L10-LW5-while-1d-game.py
@@ -2,13 +2,15 @@
 # ~~~x~~W~~~
 
 w = 4
-
+x = 2
 print()
 
 n = 1
 while n <= 10:
     if n == w:
         print("W", end = " ")
+    elif n == x:
+        print("x", end = " ")
     else:
         print("~", end = " ")
     n += 1
