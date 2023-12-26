246f256389f15fa497f121c1610685888f02d974
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-26 22:07:40 +0200

    2: das Shif wird hinzugefügt

--- L10-LW5-while-1d-game.py
+++ L10-LW5-while-1d-game.py
@@ -1,9 +1,17 @@
 # 1d game
 # ~~~x~~W~~~
 
+w = 4
+
 print()
+
 n = 1
 while n <= 10:
-    print("~", end = " ")
+    if n == w:
+        print("W", end = " ")
+    else:
+        print("~", end = " ")
     n += 1
+
+
 print("\n")
