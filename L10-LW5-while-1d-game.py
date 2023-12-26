7a999e97bd4170d80cec16b3941f5523fb1424b7
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-26 22:30:56 +0200

    5: enter direction(a,d)

--- L10-LW5-while-1d-game.py
+++ L10-LW5-while-1d-game.py
@@ -4,18 +4,29 @@
 w = 8
 x = 2
 
-# ######## DRAW MAP ########
-print()
+while True:
+    # ######## DRAW MAP ########
+    print()
 
-n = 1
-while n <= 10:
-    if n == w:
-        print("W", end = " ")
-    elif n == x:
-        print("x", end = " ")
-    else:
-        print("~", end = " ")
-    n += 1
+    n = 1
+    while n <= 10:
+        if n == w:
+            print("W", end = " ")
+        elif n == x:
+            print("x", end = " ")
+        else:
+            print("~", end = " ")
+        n += 1
 
-print("\n")
-# ######## DRAW MAP ########
+    print("\n")
+    # ######## DRAW MAP ########
+
+    # ####### INTERACTION ######
+    direction = input("enter direction(a,d): ")
+    # HW1: add limits conditions
+    # HW2: add condition mine, "BOOOM!!!" STOP MAIN LOOP
+    if direction == "a":
+        w -= 1   # w = w - 1
+    if direction == "d":
+        w += 1   # w = w + 1
+    # ####### INTERACTION ######
