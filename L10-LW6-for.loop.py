b458d6fa8d274a6306202c427fdaef305c9600ab
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-12-30 22:25:58 +0200

    1

---
+++ L10-LW6-for.loop.py
@@ -0,0 +1,13 @@
+# for
+
+# for <variable> in <iterable>:  #str, list, dict, ...
+# CODE_TO_REPEAT
+# pass / continue
+
+
+# ####### 1 ... 10
+
+for n in range(1,11):
+    print(n)
+
+# HW1: 1d robo game PRINT MAP -> "for in"
