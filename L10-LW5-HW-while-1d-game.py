85a5da42f4dd142151dfd8f4f56b004c20f70ae6
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2024-01-02 22:10:41 +0200

    HW1

---
+++ L10-LW5-HW-while-1d-game.py
@@ -0,0 +1,41 @@
+# 1d game
+# ~~~x~~W~~~
+
+loop = True
+w = 8
+x = 2
+
+while loop:
+    # ######## DRAW MAP ########
+    if w == x:
+        print("""
+              
+!!BOOOM!!
+ 🌪🌪
+~ X ~ ~ ~ ~ ~ ~ ~ ~
+""")
+        loop = False
+    else:
+        print()
+        n = 1
+        while n <= 10:
+            if n == x:
+                print("x", end = " ")
+            elif n == w:
+                print("W", end = " ")
+            else:
+                print("~", end = " ")
+            n += 1
+
+        print("\n")
+        # ######## DRAW MAP ########
+
+        # ####### INTERACTION ######
+        direction = input("enter direction(a,d): ")
+        # HW1: add limits conditions
+        # HW2: add condition mine, "BOOOM!!!" STOP MAIN LOOP
+        if direction == "a":
+            w -= 1   # w = w - 1
+        if direction == "d":
+            w += 1   # w = w + 1
+        # ####### INTERACTION ######
